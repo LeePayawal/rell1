9a0185c1e69f08d4ab158979920e1ae374c454cf
LeePayawal <leejusthinpayawal@gmail.com>
2025-07-23 20:33:48 +0800

    redeploy

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -2,9 +2,8 @@ import { SignedIn, SignedOut, SignInButton, SignOutButton, UserButton } from "@c
 
 export function TopNav(){
     return(
-      <nav className="flex w-full items-center justify-between border-b p-4 text-1 
-      font-semibold">
-        <div>RELL1</div>
+      <nav className="flex w-full items-center justify-between border-b border-zinc-800 bg-black p-4 font-semibold text-white">
+        <div >RELL1</div>
         <div>
             <SignedOut>
                 <div className="cursor-pointer">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import { UploadDialog } from "./_components/upload-dialog";
 async function Images(){
 
 const mockUrl = ["https://tr.rbxcdn.com/30DAY-Avatar-0B6FE57B08B32CA517E6F1B01C7D5A62-Png/352/352/Avatar/Png/noFilter", 
-  "https://e1.pngegg.com/pngimages/341/966/png-clipart-black-resourcesforbitches-marlboro-cigarette-box-thumbnail.png",
+  "https://www.paramountshop.com/cdn/shop/files/spongebob-squarepants-handsome-squidward-life-size-standee-309957_grande.jpg?v=1733556648",
   "https://a3dmf5xzqq.ufs.sh/f/JjoxSwTodBmpIImOlbMeTYzgH30d5mXS9QMKWskfpwcR7VhA",
 "https://a3dmf5xzqq.ufs.sh/f/JjoxSwTodBmp6xuQ8gZoYBhZ90jHAk7iCLwUuQztMbTrpSc4"];
 
@@ -17,42 +17,52 @@ const images = mockUrl.map((url, index) => ({
   url,
 })); 
 
-  return (
-    <div>
-      <div className="flex justify-end p-4">
-         <UploadDialog />
+ return (
+    <div className="w-full max-w-7xl mx-auto">
+      {/* Upload button area */}
+      <div className="flex justify-end px-4 py-6">
+        <UploadDialog />
       </div>
-  <div className="flex flex-wrap justify-center gap-6 p-4">
-    {images.map((image) => (
-    <div key = {image.id} className=" flex w-64 flex-col">
-      <div className="relative aspect-video bg-zinc-900">
-     <img 
-     src ={image.url} 
-     alt={`Image ${image.id}`} 
-     className="h-full w-full object-contain object-top"
-     />
-     </div>
-      <div className="texr-center">{image.id} </div>
+
+      {/* Image Grid */}
+      <div className="flex flex-wrap justify-center gap-6 px-4 pb-10">
+        {images.map((image) => (
+          <div
+            key={image.id}
+            className="w-64 flex flex-col rounded-lg overflow-hidden shadow-md border border-gray-200 bg-white hover:shadow-xl transition-shadow duration-300"
+          >
+            <div className="relative aspect-video bg-zinc-900">
+              <img
+                src={image.url}
+                alt={`Image ${image.id}`}
+                className="h-full w-full object-contain object-center"
+              />
+            </div>
+            <div className="text-center py-2 text-sm font-medium text-gray-700">
+              Image #{image.id}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-  ))}
-  </div>
-  </div>
   );
 }
+
 export default function HomePage() {
   return (
-    <main className="">
+    <main className="min-h-screen bg-gray-500">
       <SignedOut>
-        <div className="h-full w-full text-center text-2x1">
-          Please Sign In Above To Continue!!! 
+        <div className="flex h-screen items-center justify-center text-center text-2xl text-gray-500">
+          Please Sign In Above To Continue!!!
         </div>
       </SignedOut>
+
       <SignedIn>
-        <div className="h-full w-full text-center text-2x1">
-          Welcome Back! 
-          <Images/>
+        <div className="text-center text-3xl font-bold text-gray-800 pt-6 pb-2">
+          Welcome Back!
         </div>
+        <Images />
       </SignedIn>
-   </main>
+    </main>
   );
 }
